ce6faddba7fad65c80a3bc088e7a4ef43413348f
Fangshuai Cao <caotudou@odianyun.com>
2021-06-19 23:38:08 +0800

    add babel eslint

--- app.ts
+++ app.ts
@@ -1,11 +1,11 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 function component() {
     const element = document.createElement('div');
-  
+
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.innerHTML = ['Hello', 'webpack','angular'].join(' ');
+
     return element;
   }
-  
+  console.log('I get called from app.ts!');
   document.body.appendChild(component());
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,19 +1,58 @@
 const path = require('path');
-
+const HtmlWebpackPlugin=require('html-webpack-plugin');
 module.exports = {
-  entry: './app.ts',
+  mode: 'development',
   devtool: "inline-source-map",
+  devServer:{
+    contentBase:'./../dist'
+  },
+  entry: {app:'./app.ts'},
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, '../dist'),
+    clean: true
   },
   resolve:{
     extensions:[".ts", ".tsx", ".js"]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '管理输出',
+    })
+  ],
+  
   module: {
     rules: [
+      {
+        test: /\.s[c|a]ss$/i,
+        use: [
+            // 将 JS 字符串生成为 style 节点
+            'style-loader',
+            // 将 CSS 转化成 CommonJS 模块
+            'css-loader',
+            // 将 Sass 编译成 CSS
+            'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
       { test: /\.tsx?$/, loader: "ts-loader" }
     ]
-  }
+  },
+ 
 };
